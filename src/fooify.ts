d2bd63a45bfda837c1eea437df704a0adf7a9e1e
Akos Kitta <kittaakos@gmail.com>
2018-12-15 12:29:59 +0100

    x
    Signed-off-by: Akos Kitta <kittaakos@gmail.com>

--- src/fooify.ts
+++
@@ -1,6 +0,0 @@
-export default function (what: string | undefined | null): string | undefined | null {
-    if (typeof what === 'string') {
-        return `${what} - foo`;
-    }
-    return what;
-}
